d198d293889063763615710073914cfcb22bb94e
fhugoduarte <h.duarte222@gmail.com>
2019-09-14 10:59:13 -0300

    feat: Reset password

--- start/routes.js
+++ start/routes.js
@@ -4,4 +4,6 @@ const Route = use("Route");
 
 Route.post("users", "UserController.store");
 Route.post("sessions", "SessionController.store");
+
 Route.post("passwords", "ForgotPasswordController.store");
+Route.put("passwords", "ForgotPasswordController.update");
