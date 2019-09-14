8b8ea37c525c30f16b69094101a70b2a296d356a
fhugoduarte <h.duarte222@gmail.com>
2019-09-14 11:16:26 -0300

    chore: File upload

--- start/routes.js
+++ start/routes.js
@@ -7,3 +7,6 @@ Route.post("sessions", "SessionController.store");
 
 Route.post("passwords", "ForgotPasswordController.store");
 Route.put("passwords", "ForgotPasswordController.update");
+
+Route.get("files/:id", "FileController.show");
+Route.post("files", "FileController.store");
