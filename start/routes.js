be7ce56c41c4eada3dbf8a8154ca9a0e9f1b2cdd
fhugoduarte <h.duarte222@gmail.com>
2019-09-14 10:03:20 -0300

    Initial commit

---
+++ start/routes.js
@@ -0,0 +1,7 @@
+"use strict";
+
+const Route = use("Route");
+
+Route.post("users", "UserController.store");
+Route.post("sessions", "SessionController.store");
+Route.post("passwords", "ForgotPasswordController.store");
